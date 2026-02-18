5a569e33334edb379f71b2e9199e626760b611ca
Manuel Eberl <manuel@pruvisto.org>
2026-02-18 20:22:22 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # clop
-A tool to count lines of Isabelle proof developments
+
+This is an experimental tool to count the number of ‘proper’ lines (i.e. no blank lines and no comments) of an Isabelle proof development. Inspired by the tool `cloc`, which works for many programming languages and is extensible, but not extensible enough to handle Isabelle's nested cartouches.
+
+
+## Usage
+
+`clop` takes as an argument a list of files and directories. It then counts the lines of all files that were either given directly or that are located in any (recursive) subdirectory of a given directory. The results are displayed grouped by directory, plus possibly an ‘Unsorted’ group for files that were given directly. A grand total is also displayed at the end.
+
+
+## Scope
+
+`clop` iteratively removes blank lines and things that are considered comments. A comment is either an actual Isabelle comment (i.e. `(* ... *)`) or one of the document commands (`text ‹…›`, `section ‹…›`, etc). Nested comments and nested cartouches inside document commands are supported and should be treated correctly.
+
+The full list of document commands that are treated as comments is:
+ - `\<comment>`
+ - `txt`
+ - `text`
+ - `chapter`
+ - `section`
+ - `subsection`
+ - `subsubsection`
+ - `paragraph`
+ - `subparagraph`
+ 
+
+## Disclaimer
+ 
+Note that since `clop` uses ad-hoc regular expressions rather than Isabelle's own lexer (which a more robust tool ought to do), this is all somewhat brittle and may not give accurate results in all cases.
+
+More generally, this tool is just a quick-and-dirty experiment and has not been thoroughly tested. Use at your own peril.
+
+Feel free to create issues or pull requests.
+
+
---
+++ clop.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+import regex
+import sys
+import os
+
+re_newline_beginning = regex.compile(r'^\s*\n')
+re_double_newline = regex.compile(r'\n\s*\n')
+
+def remove_regex_occs(rs, s):
+    l_old = len(s)
+    while True:
+        for r in rs:
+            s = r.sub("", s)
+        l = len(s)
+        if l == l_old: break
+        l_old = l
+
+        s = re_newline_beginning.sub('', s)
+        while True:
+            s = re_double_newline.sub('\n', s)
+            l = len(s)
+            if l == l_old: break
+            l_old = l
+    return s
+
+re_text = regex.compile(r'(^|\s)(?:\\<comment>|txt|text|chapter|section|subsection|subsubsection|paragraph|subparagraph)\s*(?<cartouche>\\<open>(?:(?:(?!\\<open>|\\<close>).)+|(?&cartouche))*\\<close>)', regex.DOTALL)
+re_comment = regex.compile(r'\(\*((?:(?!\(\*|\*\)).)+|(?R))*\*\)', regex.DOTALL)
+regexes = [re_comment, re_text]
+
+grand_total = 0
+
+def handle_files(header, paths):
+    results = dict()
+    maxwidth1 = 10
+    maxwidth2 = 1
+    total = 0
+    for path in paths:
+        with open(path, 'r') as f:
+            s = f.read()
+            s = remove_regex_occs(regexes, s)
+            lc = len(s.splitlines())
+            fn = os.path.basename(path)
+            if fn[-4:] == '.thy': fn = fn[:-4]
+            results[path] = lc
+            maxwidth1 = max(maxwidth1, len(fn)+5)
+            maxwidth2 = max(maxwidth2, len(str(lc)))
+            total += lc
+
+    print((maxwidth1 + maxwidth2) * '-')            
+    print(header)
+    print((maxwidth1 + maxwidth2) * '-')            
+    for path, lc in sorted(results.items(), key = lambda item: item[0	]):
+        fn = os.path.basename(path)
+        if fn[-4:] == '.thy': fn = fn[:-4]
+        print ('{}{}{}'.format(fn, (maxwidth1 + maxwidth2 - len(fn) - len(str(lc))) * ' ', lc))
+        
+    print((maxwidth1 + maxwidth2) * '-')
+    print('Total{}{}'.format((maxwidth1 + maxwidth2 - 5 - len(str(total))) * ' ', total))
+    print((maxwidth1 + maxwidth2) * '-')
+    
+    global grand_total
+    grand_total += total
+
+def collect_files(path):
+    files = list()
+    for p in os.listdir(path):
+        p = os.path.join(path, p)
+        if os.path.isdir(p):
+            files += collect_files(p)
+        else:
+            if p[-4:] == '.thy': files.append(p)
+    return files
+
+def main():
+    if len(sys.argv) <= 1:
+        print('Usage: clop <thy file..>')
+        return
+    
+    files = dict()
+    for path in sys.argv[1:]:
+        if os.path.isdir(path):
+            files[path] = collect_files(path)
+        else:
+            if None not in files: files[None] = list()
+            files[None].append(path)
+
+    n = len(files)
+    for path, paths in sorted(files.items(), key = lambda item: "" if item[0] is None else item[0]):
+        if path is None:
+            header = "Unsorted"
+        else:
+            header = os.path.basename(path)
+        if not header:
+            header = os.path.dirname(path)
+        handle_files(header, paths)
+        n -= 1
+        if n > 0: print('')
+        
+    if len(files) > 1:
+        global grand_total
+        print('\nGrand total: {}'.format(grand_total))      
+
+
+
+if __name__ == "__main__":
+    main()
+
